74a865215dd97407f45ad15bf22dff43e2a75044
Colton <colton.carter1130@gmail.com>
2016-11-29 09:23:51 -0600

    sending to homepage if you log in

--- fastFoodAppliation/app/controllers/users_controller.rb
+++ fastFoodAppliation/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to '/homepage/index'
     else
       redirect_to '/signup'
     end
